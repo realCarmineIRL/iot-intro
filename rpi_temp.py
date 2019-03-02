f26ca74aa850c37298861773562be4dba6eeb360
realCarmineIRL <calicchiocarmine@gmail.com>
2019-03-02 18:06:59 +0000

    fixing content

--- rpi_temp.py
+++ rpi_temp.py
@@ -5,12 +5,10 @@ import json
 
 port = 7
 
-url = "https://dweet.io:443/dweet/for/test1-car-skerries-2019?content="
+url = "https://dweet.io:443/dweet/for/test1-car-skerries-2019"
 
 while True:
     [temp, hum] = dht(port, 0)
-    payload = '{"temperature": "%s", "humumidity": "%s"}' % (temp, hum)
-    print(json.dumps(payload))
-    r = requests.post(url + payload)
+    r = requests.post(url, json={"temperature": temp, "humidity": hum})
     print(r.text)
     time.sleep(2)
