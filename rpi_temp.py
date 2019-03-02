975ce4b3ee2b45ef4a11ac0c8d0f679d995a8e3e
realCarmineIRL <calicchiocarmine@gmail.com>
2019-03-02 17:43:42 +0000

    posting to dweet.io

--- rpi_temp.py
+++ rpi_temp.py
@@ -1,9 +1,16 @@
 from grovepi import *
 import time
+import requests
+import json
 
 port = 7
 
+url = "https://dweet.io:443/dweet/for/test1-car-skerries-2019?content="
+
 while True:
     [temp, hum] = dht(port, 0)
-    print "temp: %s, hum: %s" % (temp, hum)
+    payload = '{"temperature": "%s", "humumidity": "%s"}' % (temp, hum)
+    print(json.dumps(payload))
+    r = requests.post(url + payload)
+    print(r.text)
     time.sleep(2)
