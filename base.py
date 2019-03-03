ce9826e3cb95df788751c12cfc3fde0ac2bb7440
realCarmineIRL <calicchiocarmine@gmail.com>
2019-03-03 15:56:24 +0000

    adding DB URL as config param

--- base.py
+++ base.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import yaml
 
-engine = create_engine('sqlite:///sensors.db:memory:', echo=True)
+with open('config.yml', 'r') as c:
+    conf = yaml.load(c)
+
+db_url = conf['DB']
+
+engine = create_engine(db_url, echo=False)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from base import Session
 from sensor_db import SensorData
+import yaml
 
-engine = create_engine('sqlite:///sensors.db:memory:', echo=True)
+with open('config.yml', 'r') as c:
+    conf = yaml.load(c)
 
+db_url = conf['DB']
+
+engine = create_engine(db_url, echo=False)
 session = Session()
 
 data = session.query(SensorData).all()
